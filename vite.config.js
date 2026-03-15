a65aba96a46509b52620847fe447db57e7224f37
Rajat Kumar <rkumarz374@gmail.com>
2026-03-15 22:05:34 +0530

    Reset update

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  optimizeDeps: {
+    force: true
+  },
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
